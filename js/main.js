cff3977de3d6b7f4f7e6d77087d16c16af09d54e
sahilmhatrre001 <Sahilmhatre001@gmail.com>
2020-03-24 16:18:32 +0530

    map changes and improved response speed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+
+
+
+
+var map = {};
+
 function load() {
     var table = $('#dtBasicExample').DataTable({
         paging: false
@@ -20,8 +26,14 @@ function load() {
             var confirmedCasesIndian = value['confirmedCasesIndian'];
             var confirmedCasesForeign = value['confirmedCasesForeign'];
             var total = confirmedCasesIndian + confirmedCasesForeign;
+            map[value['loc']] = total;
             table['row']['add']([value['loc'], total, confirmedCasesIndian, confirmedCasesForeign, value['deaths'], value['discharged']])['draw'](false)
         });
+        google.charts.load('current', {
+            'packages':['geochart'],
+            'mapsApiKey': 'Add your google map api key here'
+          });
+        google.charts.setOnLoadCallback(drawRegionsMap);
     });
 }
 
@@ -72,6 +84,10 @@ function getDataForChart(data) {
     if(!today_cases)
         document.getElementById('today_cases').innerHTML = 0;
     return result;
+}
+
+function getDataForMap(data){
+    
 }
 
 function getGrowthRate(res)
@@ -195,3 +211,77 @@ function chartInit(chartData) {
     var ctx = document.getElementById('canvas').getContext('2d');
     window.myLine = new Chart(ctx, config);
 }
+
+
+
+
+function check(key1){
+    if(key1 in map)
+    {
+    return map[key1];
+    }else{
+        return 0;
+    }
+  }
+
+function drawRegionsMap() {
+    console.log("called")
+    var data = google.visualization.arrayToDataTable([
+    ['State Code', 'State', 'Corona stats (COVID - 19)'],
+    ['IN-MH','Maharashtra',check('Maharashtra')],     
+    [ 'IN-UP','Uttar Pradesh', check('Uttar Pradesh')],
+    ['IN-BR','Bihar', check('Bihar')],
+    ['IN-WB','West Bengal', check('West Bengal')],
+    ['IN-MP','Madhya Pradesh', check('Madhya Pradesh')],
+    ['IN-TN','Tamil Nadu - ' + check('Tamil Nadu') + '\n Puducherry-'+ check('Puducherry'), check('Tamil Nadu') + check('Puducherry')],
+    ['IN-RJ','Rajasthan', check('Rajasthan')],
+    ['IN-KA','Karnataka', check('Karnataka')],
+    ['IN-GJ','Gujarat', check('Gujarat')],
+    ['IN-AP','Andhra Pradesh - ' + check('Andhra Pradesh') + '\n\nTelengana - ' + check('Telengana') , check('Andhra Pradesh') + check('Telengana')],
+    ['IN-OR','Odisha', check('Odisha')],
+    ['IN-KL','Kerala', check('Kerala')],
+    ['IN-JH','Jharkhand', check('Jharkhand')],
+    ['IN-AS','Assam', check('Assam')],
+    ['IN-PB','Punjab', check('Punjab')],
+    ['IN-CT','Chhattisgarh', check('Chhattisgarh')],
+    ['IN-HR','Haryana', check('Haryana')],
+    ['IN-JK','Jammu and Kashmir - ' + check('Jammu and Kashmir') + '\n Ladakh - ' +check('Ladakh') , check('Jammu and Kashmir') + check('Ladakh')],
+    ['IN-UT','Uttarakhand', check('Uttarakhand')],
+    ['IN-HP','Himachal Pradesh', check('Himachal Pradesh')],
+    ['IN-TR','Tripura',check('Tripura') ],
+    ['IN-ML','Meghalaya', check('Meghalaya')],
+    ['IN-MN','Manipur', check('Manipur')],
+    ['IN-NL','Nagaland', check('Nagaland')],
+    ['IN-GA','Goa', check('Goa')],
+    ['IN-AR', 'Arunachal Pradesh', check('Arunachal Pradesh')],
+    ['IN-MZ','Mizoram', check('Mizoram')],
+    ['IN-SK','Sikkim', check('Sikkim')],
+    ['IN-DL','Delhi', check('Delhi')],
+    ['IN-PY','Puducherry', check('Puducherry')],
+    ['IN-CH','Chandigarh', check('Chandigarh')],
+    ['IN-AN','Andaman and Nicobar', Math.max(check('Andaman and Nicobar Islands'),check('Andaman and Nicobar'))],
+    ['IN-DN','Dadra and Nagar Haveli', check('Dadra and Nagar Haveli')],
+    ['IN-DD','Daman and Diu', check('Daman and Diu')],
+    ['IN-LD','Lakshadweep', check('Lakshadweep')]
+    ]);
+
+    var options = {
+        region: 'IN',
+        domain:'IN',
+        resolution: 'provinces',
+        datalessRegionColor: 'transparent',
+        is3D : true,
+        keepAspectRatio: true,
+    };
+
+    options['colorAxis'] = {colors : ['#ffffff','#f54c4c']};
+    
+
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+    
+    chart.draw(data, options);
+}
+
+$(window).resize(function(){
+    drawRegionsMap();
+});
